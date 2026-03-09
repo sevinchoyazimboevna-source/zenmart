970adbe17c4945c3bc47e2d7a90caab7990e705b
Sevinchoy <sevinchoyazimboyevnaa@gmail.com>
2026-03-09 15:40:08 +0500

    feat: completed view

---
+++ script.js/scripts.js
@@ -0,0 +1,9 @@
+ const swiper = new Swiper(".mySwiper", {
+    slidesPerView:4,
+    spaceBetween:20,
+
+    navigation:{
+        nextE1: ".swiper-button-next",
+        prevE1: "swiper-button-prev",
+    },
+ });
